3c8b6fd5f7f3cbfaf9a076f88f20b78605cf5438
Matheus Campos <theuscampos45@gmail.com>
2022-02-13 21:32:11 -0300

    Projeto completo

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+SECRET_KEY = 'alura'
+# Configure de acordo com seu banco
+MYSQL_HOST = ""
+MYSQL_USER = ""
+MYSQL_PASSWORD = ""
+MYSQL_DB = "jogoteca"
+MYSQL_PORT =
+
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
